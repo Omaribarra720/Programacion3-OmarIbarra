d1a730a87769d50274fa7b9484cab954ca5e7fcb
Omaribarra720 <159938659+Omaribarra720@users.noreply.github.com>
2024-03-20 12:10:51 -0700

    unidad 3 ejercicio 15

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,20 @@
-
+import java.awt.EventQueue;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Ventana ventana= new Ventana();
-		
+		//Ventana ventana= new Ventana();
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TicTac frame = new TicTac();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 
 	}
 

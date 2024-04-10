976b0d2e4e837744878b5e03a358495e8cce4b3b
Omaribarra720 <159938659+Omaribarra720@users.noreply.github.com>
2024-04-09 18:06:39 -0700

    unidad 3 ejercicio 21

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Numbers frame = new Numbers();
+					Registro frame = new Registro();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
